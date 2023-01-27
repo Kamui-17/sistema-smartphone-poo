2c543ddc939de7fb45be3486230c814b8167ee46
Matheus Almeida <iamthedge@gmail.com>
2023-01-27 03:32:17 -0300

    Commit Atualização.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia:");
+Nokia nokia = new Nokia(numero: "123", modelo: "Modelo 1", imei: "111111", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular iPhone:");
+Iphone iphone = new Iphone(numero: "456", modelo: "Modelo 2", imei: "222222", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
